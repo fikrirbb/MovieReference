a64c542a4be4c68cfd6a902478dacfea0643856b
Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2017-08-06 06:55:24 +0800

    Create SQLiteHelper class

---
+++ app/src/main/java/com/example/satyakresna/moviereference/database/FavoriteDBHelper.java
@@ -0,0 +1,40 @@
+package com.example.satyakresna.moviereference.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by satyakresna on 06-Aug-17.
+ */
+
+public class FavoriteDBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "favorites.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public FavoriteDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_TABLE = "CREATE TABLE "+ FavoriteContract.FavoriteEntry.TABLE_NAME + " (" +
+                FavoriteContract.FavoriteEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                FavoriteContract.FavoriteEntry.COLUMN_MOVIE_ID + " INTEGER NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_TITLE + " TEXT NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_POSTER + " TEXT NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_SYNOPSIS + " TEXT NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_RATING + " REAL NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_RELEASE_DATE + " TEXT NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_BACKDROP + " TEXT NOT NULL, " +
+                FavoriteContract.FavoriteEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                "); ";
+        sqLiteDatabase.execSQL(SQL_CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ FavoriteContract.FavoriteEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
