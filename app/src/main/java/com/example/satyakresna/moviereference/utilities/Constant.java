3c1e6876848a0970e909e2f8b6f086dbce8147d5
Satya Kresna Adi Pratama <satyakresna6295@gmail.com>
2017-08-05 11:57:46 +0800

    Update Constant class

--- app/src/main/java/com/example/satyakresna/moviereference/utilities/Constant.java
+++ app/src/main/java/com/example/satyakresna/moviereference/utilities/Constant.java
@@ -7,8 +7,7 @@ import com.example.satyakresna.moviereference.BuildConfig;
  */
 
 public class Constant {
-    public static final String URL_API = "http://api.themoviedb.org/3/";
-    public static final String MOVIE = "movie/";
+    public static final String URL_API = "http://api.themoviedb.org/3/movie/";
     public static final String TOP_RATED = "top_rated";
     public static final String POPULAR = "popular";
     public static final String API_KEY = BuildConfig.API_KEY;
